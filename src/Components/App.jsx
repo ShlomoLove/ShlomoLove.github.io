08c13c8fb712180171b6e21c2bc6434d5e97f4c7
Scott Nassau <snassau@gmail.com>
2019-09-20 23:28:24 -0700

    create architecture for React build

---
+++ src/Components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <div>
+        <div>WELCOME TO SCOTT NASSAU'S PAGE</div>
+      </div>
+    )
+  }
+}
+
+export default App; 
