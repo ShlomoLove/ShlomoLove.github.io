ef74e2e7312e4716f6dcd03b707ac322d502d6fc
Scott Nassau <snassau@gmail.com>
2019-10-08 21:36:36 -0700

    add About text

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const About = props => {
   return (
     <div className='headerContainer'>
-      <div className='frontCopy1'>Hello About</div>
+      <div className='aboutText'>For many years running a marathon had been on my bucket list, but simply thinking about running a marathon wasn’t going to make it happen. It required action. I arduously trained for the Los Angeles Marathon and challenged myself to finish it in under 4 hours. Not only did I shatter my goal by 30 minutes, but I caught the the running bug. A year later, I set out to push my limits further and run 65 kilometers (40 miles). Yet after 30 miles, I hit the wall. My body ached and I was on the verge of giving up, but tenaciously I put one foot in front of the other and continued to run. Running has become a metaphor for my life. 
+</div>
+      <div className='aboutText'>I began my career in linguistics, studying ancient Near Eastern languages. While in the midst of a Ph.D. program, I courageously ventured out and launched a non-profit startup focused on building a safe-environment to promote holistic growth, human connection and the welfare of our city. This venture required me to spearhead and engage in the development and design of the organization’s PHP based website, built with Wordpress. For many years I had been interested in technology, web development and design. My initial fascination began with the emergence of blogging and how it enabled me to  share my thoughts and ideas in a way unimagined in previous generations.
+</div>
+      <div className='aboutText'>The time I had spent in undergraduate and graduate school examining the intricacies of unspoken languages instilled in me a love for the art and science of interpreting evolving forms of written communication. As a result, I found a natural transition into coding, which simply is the 21st Century version of writing an unspoken language. Just like ancient languages, coding utilizes a structured syntax, is constantly evolving, borrows from adjacent languages and moves toward clarity. 
+</div>
     </div>
   )
 }
