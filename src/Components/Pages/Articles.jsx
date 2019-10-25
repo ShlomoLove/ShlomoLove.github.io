f44235f5a8d7c5b01fcb46a15e2c87d567639c48
Scott Nassau <snassau@gmail.com>
2019-10-25 16:44:10 -0700

    update fonts

--- src/Components/Pages/Articles.jsx
+++ src/Components/Pages/Articles.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Articles = props => {
   return (
     <div className='headerContainer'>
-      <div className='frontCopy1'>Coming Soon: Articles</div>
+      <div className='articlesTitle'>Coming Soon</div>
     </div>
   )
 }
